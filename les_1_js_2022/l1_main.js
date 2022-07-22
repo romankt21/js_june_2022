214f7bd3f5ef6ccd04d550160a9f2669f70e0016
romankt21 <romankt21@gmail.com>
2022-07-23 00:18:05 +0300

    Initial commit

---
+++ les_1_js_2022/l1_main.js
@@ -0,0 +1,39 @@
+// console.log(12)
+//
+// let a = 100;
+// console.log(a);
+// a = 200;
+
+// let a = 20;
+// console.log(a);
+//
+// let str1 = 'hello';
+// let str2 = 'okten';
+// let words = str1 + str2;
+// console.log(words);
+//
+// console.log(5 === 5);
+
+
+// let s = '!!!';
+// let n1 = 10;
+// let n2 = 20;
+// console.log(n1 + n2 + s);
+// console.log(s + n1 + n2);
+// console.log(s + (n1 + n2));
+
+
+// console.log(100-true);
+//
+//
+// console.log('!'+true);
+
+// console.log(+true);
+// console.log(+'123');
+
+// console.log(!!'qqw')
+//
+// console.log(!!'')
+
+console.log('10'+1);
+console.log('10'-1);
