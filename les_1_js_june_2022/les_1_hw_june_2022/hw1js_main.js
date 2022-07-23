ad8296a49fb34fed24acd68fa81542f77e0d4b93
romankt21 <romankt21@gmail.com>
2022-07-23 21:36:34 +0300

    change

---
+++ les_1_js_june_2022/les_1_hw_june_2022/hw1js_main.js
@@ -0,0 +1,105 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1,
+// 10, -999, 123, 3.14, 2.7, 16, true, false.
+
+// let str1 = 'hello';
+// let str2 = 'owu';
+// let str3 = 'com';
+// let str4 = 'ua';
+// let num1 = 1;
+// let num2 = 10;
+// let num3 = -999;
+// let num4 = 123;
+// let num5 = 3.14;
+// let num6 = 2.7;
+// let num7 = 16;
+// let bul1= true;
+// let bul2 = false;
+
+
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+// console.log(str1);
+// alert(str1);
+// document.write(str1);
+//
+// console.log(str2);
+// alert(str2);
+// document.write(str2);
+//
+// console.log(str3);
+// alert(str3);
+// document.write(str3);
+//
+// console.log(str4);
+// alert(str4);
+// document.write(str4);
+//
+// console.log(num1);
+// alert(num1);
+// document.write(num1);
+//
+// console.log(num2);
+// alert(num2);
+// document.write(num2);
+//
+// console.log(num3);
+// alert(num3);
+// document.write(num3);
+//
+// console.log(num4);
+// alert(num4);
+// document.write(num4);
+//
+// console.log(num5);
+// alert(num5);
+// document.write(num5);
+//
+// console.log(num6);
+// alert(num6);
+// document.write(num6);
+//
+// console.log(num7);
+// alert(num7);
+// document.write(num7);
+//
+// console.log(bul1);
+// alert(bul1);
+// document.write(bul1)
+//
+// console.log(bul2);
+// alert(bul2);
+// document.write(bul2)
+
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+// let firstName;
+// firstName = 'Roman';
+//
+// let middleName;
+// middleName = 'Ihorovich';
+//
+// let lastNime;
+// lastNime = 'Kutnyi';
+//
+// let person = firstName + ' ' + middleName + ' ' + lastNime;
+// console.log(person);
+//
+// let person1 = `${firstName} ${middleName} ${lastNime}`;
+// console.log(person1)
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+// let a = 100;
+// console.log(typeof a);
+//
+// let b = '100';
+// console.log(typeof b);
+//
+// let c = true;
+// console.log(typeof c);
+
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+console.log(prompt("прошу ввести ім'я"))
+
+console.log(prompt("прошу ввести по-батькові"));
+
+console.log(prompt("прошу ввести прізвище"));
