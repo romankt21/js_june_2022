840f4177dfc744ebb78b1506800e9230f66a05f6
romankt21 <romankt21@gmail.com>
2022-07-29 19:38:43 +0300

    change

---
+++ les_3_js_june_2022/les_3_lec_js_june_2022/l3js_main.js
@@ -0,0 +1,63 @@
+// let answer =  + prompt('what is your age');
+// console.log(answer, typeof answer);
+
+// let age = +prompt('what is your age?');
+// if (age >= 18) {
+//     console.log('show film');
+// } else if (age < 18 && age >=8) {
+//     console.log('show bethoven');
+// } else if ( age < 8) {
+//     console.log('show cartoon')
+// }
+
+// let color = prompt('what do you see?');
+// if (color === 'green') {
+//     console.log('you can go');
+// } else if (color === 'yellow') {
+//     console.log('wait');
+// } else if (color === 'red') {
+//     console.log('stop');
+// } else {
+//     console.log('be careful')
+// }
+
+
+// let color = prompt('what do you see?');
+//
+// switch (color) {
+//     case 'green':
+//         console.log('go');
+//         break;
+//
+//     case 'yellow':
+//         console.log('wait');
+//         break
+//
+//     case 'red':
+//         console.log('stop');
+//         break;
+//     default:
+//         console.log('?????')
+//
+// }
+
+
+// switch ('blue') {
+//     case 'green':
+//     case 'light green':
+//     case 'blue':
+//
+//         console.log('go');
+//         break;
+//
+//     case 'yellow':
+//         console.log('wait');
+//         break;
+//
+//     case 'red':
+//         console.log('stop');
+//         break;
+//     default:
+//         console.log('?????')
+//
+// }
