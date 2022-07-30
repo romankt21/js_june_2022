9adf24ce8af3caa81af6e08e8286df3a38910387
romankt21 <romankt21@gmail.com>
2022-07-30 13:09:31 +0300

    change

--- les_3_js_june_2022/les_3_lec_js_june_2022/l3js_main.js
+++ les_3_js_june_2022/les_3_lec_js_june_2022/l3js_main.js
@@ -62,3 +62,67 @@
 //         console.log('?????')
 //
 // }
+
+
+// let b = confirm('do some');
+// let x;
+//
+// if (b) {
+//     x =100;
+// } else {
+//     x = 200;
+// }
+//
+// console.log(x);
+
+// let b = confirm('do some');
+// let x = b ? 100 : 200;
+// console.log(x);
+
+// let b = confirm('do some');
+// let x = b ? 100 : b > 100 ? 'asd' : 'qwe';
+// console.log(x);
+
+// if ('') {
+//     console.log('+')
+// } else {
+//     console.log('-')
+// }
+
+// let user = {
+//     name: 'vasyl',
+//     age: 31,
+//     // status: 'true'
+// }
+//
+// console.log(user.status);
+// if (user.status) {
+//     if (user.age > 30) {
+//         console.log('old')
+//     }
+// }
+
+// let user = {
+//     name: 'vasyl',
+//     age: 31,
+//     status: 'true'
+// }
+//
+// console.log(user.status);
+// if (user.status && user.age > 30) {
+//     console.log('old')
+//     }
+
+// let name = prompt('enter your name?')
+// console.log(name)
+//
+// if (!name) {
+//     console.log('john')
+// }
+
+// let name = prompt('enter your name?') || 'john';
+// console.log(name)
+
+let user = {}
+
+console.log(user?.email?.send())
