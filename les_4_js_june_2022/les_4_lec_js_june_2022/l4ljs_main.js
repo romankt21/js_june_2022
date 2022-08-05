e073a56e98311509acac5b827c788f5a15f6423c
romankt21 <romankt21@gmail.com>
2022-08-05 19:26:23 +0300

    change

--- les_4_js_june_2022/les_4_lec_js_june_2022/l4ljs_main.js
+++ les_4_js_june_2022/les_4_lec_js_june_2022/l4ljs_main.js
@@ -171,36 +171,6 @@
 //     console.log('qwer')
 // }  while (false)
 
-let users = [
-    {name: 'vasyl', age: 31, status: false},
-    {name: 'petro', age: 30, status: true},
-    {name: 'mykola', age: 29, status: true},
-    {name: 'olya', age: 28, status: false},
-    {name: 'max', age: 30, status: true},
-    {name: 'ganna', age: 31, status: false},
-    {name: 'oleg', age: 28, status: false},
-    {name: 'andriy', age: 29, status: true},
-    {name: 'maria', age: 30, status: true},
-    {name: 'olya', age: 31, status: false},
-    {name: 'max', age: 31, status: true},
-]
-
-
-let i = 0;
-do {
-    console.log(users[i]);
-    i++;
-    } while (i < users.length)
-
-
-
-
-// let user = {name: 'vasyl', age: 31, status: false, email: 'rt'};
-// for (let userKey in user) {
-//     // console.log(userKey);
-// console.log(userKey, user[userKey]);
-// }
-
 // let users = [
 //     {name: 'vasyl', age: 31, status: false},
 //     {name: 'petro', age: 30, status: true},
@@ -214,12 +184,43 @@ do {
 //     {name: 'olya', age: 31, status: false},
 //     {name: 'max', age: 31, status: true},
 // ]
-// for (const user of users) {
-//     document.write('<ol>');
-//     for (const userKey in user) {
-//         document.write(`<li>${userKey}- ${user[userKey]} </li>`)
-//     }
 //
 //
-//     document.write('</ol>');
+// let i = 0;
+// do {
+//     console.log(users[i]);
+//     i++;
+//     } while (i < users.length)
+
+
+
+
+// let user = {name: 'vasyl', age: 31, status: false};
+// user.email = 'krok';
+// console.log(user)
+// for (let userKey in user) {
+//   //  console.log(userKey);
+// console.log(userKey, user[userKey]);
 // }
+
+let users = [
+    {name: 'vasyl', age: 31, status: false},
+    {name: 'petro', age: 30, status: true},
+    {name: 'mykola', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'ganna', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andriy', age: 29, status: true},
+    {name: 'maria', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true},
+]
+for (const user of users) {
+    document.write('<ol>');
+    for (const userKey in user) {
+        console.log(userKey,user[userKey]);
+        document.write(`<li>${userKey}- ${user[userKey]} </li>`)
+    }
+    document.write('</ol>');
+}
