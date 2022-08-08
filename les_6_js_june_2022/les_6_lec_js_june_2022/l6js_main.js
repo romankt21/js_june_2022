4acf70fa85256089f003aae5b169440589e837cd
romankt21 <romankt21@gmail.com>
2022-08-08 22:37:22 +0300

    change

---
+++ les_6_js_june_2022/les_6_lec_js_june_2022/l6js_main.js
@@ -0,0 +1,29 @@
+// let str = 'hello okten';
+// console.log(str);
+// console.log(str[0]);
+// for (const strElement of str) {
+//     console.log(strElement);
+// }
+//
+// let s = new String('ggh');
+// console.log(typeof s);
+
+let str = 'hello Okten';
+// let s = str.concat('!!!');
+// console.log(s)
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.startsWith('he'));
+console.log(str.endsWith('en'));
+console.log(str.substring(0, 7));
+console.log(str.indexOf('h'));
+console.log(str.lastIndexOf('o'));
+console.log(str.indexOf('e', 5));
+console.log(str.charAt(8));
+// console.log(str.replace('e', '!'));
+console.log(str.replaceAll('e', '!'));
+let split = str.split(' ');
+console.log(split);
+
+
