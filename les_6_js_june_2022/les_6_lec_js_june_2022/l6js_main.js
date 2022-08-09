89f2a84296c3545271aa74b3c0313fdb1e33ed70
romankt21 <romankt21@gmail.com>
2022-08-09 22:35:17 +0300

    change

--- les_6_js_june_2022/les_6_lec_js_june_2022/l6js_main.js
+++ les_6_js_june_2022/les_6_lec_js_june_2022/l6js_main.js
@@ -8,22 +8,49 @@
 // let s = new String('ggh');
 // console.log(typeof s);
 
-let str = 'hello Okten';
-// let s = str.concat('!!!');
-// console.log(s)
+// let str = 'hello Okten';
+//  let s = str.concat('!!!');
+//  console.log(s)
+//
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str.startsWith('he'));
+// console.log(str.endsWith('en'));
+// console.log(str.substring(0, 7));
+// console.log(str.indexOf('h'));
+// console.log(str.lastIndexOf('o'));
+// console.log(str.indexOf('e', 5));
+// console.log(str.charAt(8));
+//  console.log(str.replace('e', '!'));
+// console.log(str.replaceAll('e', '!'));
+// let split = str.split(' ');
+// console.log(split);
 
-console.log(str.toUpperCase());
-console.log(str.toLowerCase());
-console.log(str.startsWith('he'));
-console.log(str.endsWith('en'));
-console.log(str.substring(0, 7));
-console.log(str.indexOf('h'));
-console.log(str.lastIndexOf('o'));
-console.log(str.indexOf('e', 5));
-console.log(str.charAt(8));
-// console.log(str.replace('e', '!'));
-console.log(str.replaceAll('e', '!'));
-let split = str.split(' ');
-console.log(split);
+let arr = [];
+// console.log(typeof arr);
+// console.log(typeof {});
+// console.log(Array.isArray());
+// console.log(Array.isArray(arr));
+
+arr[arr.length] = 100;
+arr[arr.length] = 'asd';
+
+arr.push('new element');
+arr.push('new element2');
+arr.push('new element3');
+console.log(arr);
+
+
+console.log(arr.pop());
+console.log(arr);
+
+arr.unshift('first element');
+console.log(arr);
+
+console.log(arr.unshift('first element'));
+
+arr.shift();
+console.log(arr);
+console.log(arr.shift());
 
 
